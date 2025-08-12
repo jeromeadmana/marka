7755f910406328f014b62d56817aa890f2213a9b
Jerome <jerome.admana@gmail.com>
2025-08-13 04:34:16 +0800

    User Model

---
+++ marka_api/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace marka_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Guid CustomerId { get; set; }
+    }
+}
