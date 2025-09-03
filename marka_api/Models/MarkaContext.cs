36d3b9a7ec0c469f666c7bb471ac1dfacd05844d
Jerome <jerome.admana@gmail.com>
2025-09-04 01:40:40 +0800

    new model marka context

---
+++ marka_api/Models/MarkaContext.cs
@@ -0,0 +1,14 @@
+﻿namespace marka_api.Models
+{
+    public class MarkaContext
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime? deleted_at { get; set; }
+        public Guid? deleted_by { get; set; }
+        public bool is_deleted { get; set; }
+        public Guid customer_id { get; set; }
+
+    }
+}
